c2600f6c866e56c47e9e6f398697a38500c642c4
Carlos Vega <clmvega@gmail.com>
2020-11-21 14:10:29 -0600

    bug: issue with readonly lists

--- src/components/ShareModal.js
+++ src/components/ShareModal.js
@@ -13,7 +13,7 @@ export const ShareModal = ({ showDialog, setShowDialog, onDismiss }) => {
 
   const onClose = (result) => {
     if (result === 'success') {
-      onDismiss(passphrase, readOnly);
+      onDismiss(passphrase, readOnly === 'readonly');
     }
     setShowDialog(false);
   };
