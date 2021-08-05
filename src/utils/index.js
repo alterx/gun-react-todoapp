700e2d5cc0151466099b9a2609d41eade8f1de7a
Carlos Vega <clmvega@gmail.com>
2021-08-05 11:56:00 -0600

    feat: updates Gun version and removes unused code

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,18 @@
-export const instantiateNewGun = (Gun, opts) => () => {
-  return Gun(opts);
+// key<sha1({Company:'mycompany123',companyid:321,Name:'My Company'})> or "da39a3ee5e6b4b0d3255bfef95601890afd80709" substringed to  da39a3ee5e -> value<Gun refpointer>
+
+// try await SEA.work("hey", "ho", null,{name:'SHA-1',encode:'hex'})
+
+/// or 'SHA-256'
+export const getHash = async (
+  data,
+  sea,
+  max = 12,
+  name = 'SHA-1',
+  salt = null
+) => {
+  const message = await sea.work(data, salt, null, {
+    name,
+    encode: 'hex',
+  });
+  return message.slice(0, max);
 };
--- src/utils/shared-resource.js
+++ src/utils/shared-resource.js
@@ -1,14 +1,12 @@
 import { useState } from 'react';
+import { getHash } from './index';
 
 export const createSharedResource = async (appName, newGunInstance, sea) => {
   return new Promise(async (resolve) => {
     const newGun = newGunInstance();
     const keys = await sea.pair();
     const sharedKeyString = JSON.stringify(keys);
-    let keyId = await sea.work(sharedKeyString, undefined, undefined, {
-      name: 'SHA-256',
-    });
-    keyId = keyId.slice(0, 12);
+    let keyId = await getHash(sharedKeyString, sea);
     const nodeID = `${appName}/${keyId}`;
 
     newGun.on('auth', () => {
@@ -73,36 +71,3 @@ export const useSharedResource = (appName, gun, sea) => {
 
   return [sharedResource, setSharedResource];
 };
-
-// key<sha1({Company:'mycompany123',companyid:321,Name:'My Company'})> or "da39a3ee5e6b4b0d3255bfef95601890afd80709" substringed to  da39a3ee5e -> value<Gun refpointer>
-
-// try await SEA.work("hey", "ho", null,{name:'SHA-1',encode:'hex'})
-
-/// or 'SHA-256'
-const index = async (data, sea, name = 'SHA-1', max = 10) => {
-  const message = await sea.work(JSON.stringify(data), null, null, {
-    name,
-    encode: 'hex',
-  });
-  console.log(message);
-  return message.slice(0, max);
-};
-// -> ce06092fb948d9ffac7d1a376e404b26b7575bcc11ee05a4615fef4fec3a308b
-
-export function encode(buffer) {
-  return buffer
-    .toString('base64')
-    .replace(/\+/g, '-') // Convert '+' to '-'
-    .replace(/\//g, '_') // Convert '/' to '_'
-    .replace(/=+$/, ''); // Remove ending '='
-}
-
-export function decode(base64) {
-  // Add removed at end '='
-  base64 += Array(5 - (base64.length % 4)).join('=');
-  base64 = base64
-    .replace(/\-/g, '+') // Convert '-' to '+'
-    .replace(/\_/g, '/'); // Convert '_' to '/'
-
-  return new Buffer(base64, ['base64']);
-}
