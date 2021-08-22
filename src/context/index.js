9acd207bb0aff575c09f310109ed1980f80f485e
Carlos Vega <clmvega@gmail.com>
2021-08-21 21:03:11 -0600

    chore: update @altrx/gundb-react-auth to latest version

--- src/context/index.js
+++ src/context/index.js
@@ -4,12 +4,32 @@ import Gun from 'gun';
 import sea from 'gun/sea';
 import { GunProvider } from '@altrx/gundb-react-auth';
 
+const asyncFn =
+  (fn) =>
+  (...args) => {
+    return new Promise((resolve) => {
+      resolve(fn.call(this, ...args));
+    });
+  };
+
+const storage = {
+  setItem: asyncFn(localStorage.setItem.bind(localStorage)),
+  getItem: asyncFn(localStorage.getItem.bind(localStorage)),
+  removeItem: asyncFn(localStorage.removeItem.bind(localStorage)),
+};
+
 const peers = ['https://gun-us.herokuapp.com/gun'];
 
 const AppProviders = ({ children }) => {
   return (
     <Router>
-      <GunProvider peers={peers} sea={sea} Gun={Gun} keyFieldName="todoKeys">
+      <GunProvider
+        peers={peers}
+        sea={sea}
+        Gun={Gun}
+        keyFieldName="todoKeys"
+        storage={storage}
+      >
         {children}
       </GunProvider>
     </Router>
